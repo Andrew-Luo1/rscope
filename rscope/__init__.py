d620ab169c0fdaaeb9556a2801bbe0431adee90c
Andrew Luo <andrew.luo1@outlook.com>
2025-05-17 15:05:46 -0400

    Local and SSH-remote training working, tests passing

---
+++ rscope/__init__.py
@@ -0,0 +1,15 @@
+"""
+RScope - A tool for visualizing and analyzing MuJoCo simulations.
+"""
+
+from .main import *
+from .state import *
+from .rollout import *
+from .viewer_utils import *
+from .rscope_utils import *
+from .model_loader import *
+from .image_processing import *
+from .config import *
+from .event_handler import *
+
+__version__ = "0.1.0"
